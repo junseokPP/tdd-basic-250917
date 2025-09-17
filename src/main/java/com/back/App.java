5393ce0b2a1ddd741c8edb6c5a3f1241c3bc93d7
junseokPP <pjs981130@gmail.com>
2025-09-17 23:26:41 +0900

    6강 : 0+5=5을 assertThat으로 구현 -green,blue

--- src/main/java/com/back/App.java
+++ src/main/java/com/back/App.java
@@ -2,11 +2,6 @@ package com.back;
 
 public class App {
     public int plus(int a,int b){
-
-        if(a==1 && b==2){
-            return 3;
-        }
-
-        return 30;
+        return a+b;
     }
 }
